c3b6c8f448ee318ee226c1bbf8f3cdd878e1e744
xy106xfy <xy9122000@hotmail.com>
2018-03-20 11:26:28 +0800

    version 1.0 20180319

---
+++ wxinterface.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#########################################################################
+import uuid, os, json
+from django.http import HttpResponseRedirect, JsonResponse, HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from WXBizMsgCrypt import WXBizMsgCrypt
+import xml.etree.cElementTree as ET
+import sys
+import thread
+import ZabbixWeTalkApi
+import time
+
+sToken = "hJqcu3uJ9Tn2gXPmxx2w9kkCkCE2EPYo"
+sEncodingAESKey = "6qkdMrs68nhKduznJYO1A37W2oEgpkMUvkttRToqhUt"
+sCorpID = "wwdf6ea09fe4015784"
+
+wxcpt=WXBizMsgCrypt(sToken,sEncodingAESKey,sCorpID)
+zwapi = ''
+wapi = ''
+
+def wxinterface(request):
+    global wxcpt
+    global zwapi
+    global wapi
+
+    if request.method == 'GET':
+        sVerifyMsgSig = request.GET.get('msg_signature')
+        sVerifyTimeStamp =  request.GET.get('timestamp')
+        sVerifyNonce = request.GET.get("nonce")
+        sVerifyEchoStr = request.GET.get("echostr")
+        ret,sEchoStr=wxcpt.VerifyURL(sVerifyMsgSig, sVerifyTimeStamp,sVerifyNonce,sVerifyEchoStr)
+        if(ret!=0):
+            print "ERR: VerifyURL ret: " + str(ret)
+            sys.exit(1)
+        context = {}
+        context['content'] = sEchoStr
+        return render_to_response('wxinterface/wxinterface.html',context=context)
+        #return HttpResponse(sEchoStr)
+    elif request.method == 'POST':
+        sReqMsgSig = request.GET.get('msg_signature')
+        sReqTimeStamp = request.GET.get('timestamp')
+        sReqNonce = request.GET.get("nonce")
+        sReqData = request.body
+        ret,sMsg = wxcpt.DecryptMsg(sReqData, sReqMsgSig, sReqTimeStamp, sReqNonce)
+        if( ret!=0 ):
+            print "ERR: DecryptMsg ret: " + str(ret)
+            sys.exit(1)
+        xml_tree = ET.fromstring(sMsg)
+        try:
+            toUser = xml_tree.find('ToUserName').text
+            fromUser = xml_tree.find('FromUserName').text
+            msgType = xml_tree.find('MsgType').text
+            if msgType == 'text':
+                content = xml_tree.find('Content').text
+            elif msgType == 'event':
+                content = xml_tree.find('EventKey').text
+        except Exception as e:
+            sys.exit(1)
+        context = {}
+        context['content'] = content
+        time_str = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(int(sReqTimeStamp)))
+        tmpfile = open('/opt/work/wxinterface.log','a')
+        log_content = time_str + u', 用户:' + fromUser + u', 事件类型:' + msgType + u', 命令:' + content + u' ,命令长度:' + str(len(content))
+        tmpfile.write(log_content + '\n')
+        tmpfile.close()
+        #WXEnterprise.AutoSendAlertInfo(cmd_msg=content) 
+        if not zwapi or not wapi:
+            zwapi = ZabbixWeTalkApi.ZabbixWeTalkApi(config_file='WeiXinByGroup.conf', wt_iGet=True)
+            wapi = zwapi.w_dic['WX_iGet']['wapi']
+        # 根据content内容，取相对应的信息，主动发送到组
+        para_dic = {}
+        para_dic['wapi'] = wapi
+        para_dic['cmd_msg'] = content.strip()
+        para = tuple(para_dic.values())
+        # 启用线程，调用主动发送接口
+        thread.start_new_thread(zwapi.AutoSendAlertInfo,para)
+        #zwapi.AutoSendAlertInfo(wapi=wapi, cmd_msg=content)
+        return render_to_response('wxinterface/wxinterface.html',context=context,context_instance=RequestContext(request))
+        #return HttpResponse('ok')
